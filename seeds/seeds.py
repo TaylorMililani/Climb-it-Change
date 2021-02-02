58b922cf95aa20182f2a45c81e543f0027310f49
Taylor Tompkins <taylormililani@gmail.com>
2021-02-01 16:40:42 -0800

    config file

---
+++ seeds/seeds.py
@@ -0,0 +1,13 @@
+from app import Sesh, Workout, Antagonist
+
+b_sesh = []
+i_sesh = []
+a_sesh = []
+
+b_workout = []
+i_workout = []
+a_workout = []
+
+b_antagonist = []
+i_antagonist = []
+a_antagonist = []
